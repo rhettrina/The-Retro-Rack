57da6f5f8eceabca5050f47c38105d35c0af2af1
rhettrina <michael18ricafrente@gmail.com>
2024-12-18 10:30:37 +0800

    ad

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
-        'name' => 'hachisnail/the-retro-rack',
+        'name' => 'hachisnail/retro-rack',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '62616fdc86b5d5375d178d391b2d6ffe49a80f55',
+        'reference' => '0979d9c04ea357b7776afd4f8a5de37c6bc484f3',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'dev' => true,
     ),
     'versions' => array(
-        'hachisnail/the-retro-rack' => array(
+        'hachisnail/retro-rack' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '62616fdc86b5d5375d178d391b2d6ffe49a80f55',
+            'reference' => '0979d9c04ea357b7776afd4f8a5de37c6bc484f3',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
