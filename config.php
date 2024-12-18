4a6f62d09fb2cdbe7d7b80dd940532040c7c0d31
rhettrina <michael18ricafrente@gmail.com>
2024-12-18 11:02:01 +0800

    asd

--- config.php
+++ config.php
@@ -1,10 +1,11 @@
+//
 <?php
 
 
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'retrorack');
+define('DB_USERNAME', 'u143688490_wiz');
+define('DB_PASSWORD', 'Louis@18');
+define('DB_NAME', 'u143688490_retrorack');
 
 
 $conn = mysqli_connect(
@@ -18,22 +19,3 @@ if ($conn == false)
     die('Error: Cannot connect');
 
 
-
-//     <?php
-
-
-// define('DB_SERVER', 'localhost');
-// define('DB_USERNAME', 'u143688490_wiz');
-// define('DB_PASSWORD', 'Louis@18');
-// define('DB_NAME', 'u143688490_retrorack');
-
-
-// $conn = mysqli_connect(
-//     DB_SERVER,
-//     DB_USERNAME,
-//     DB_PASSWORD,
-//     DB_NAME
-// );
-
-// if ($conn == false)
-//     die('Error: Cannot connect');
